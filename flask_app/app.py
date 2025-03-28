f1508cc6cf0c566e79b554fbe9f1d2acfd45bacb
Ahmed Mevawala <uveshmevawala1989@gmail.com>
2025-03-28 15:28:36 +0000

    Added ECR code

--- flask_app/app.py
+++ flask_app/app.py
@@ -115,7 +115,7 @@ PREDICTION_COUNT = Counter(
 model_name = "my_model"
 def get_latest_model_version(model_name):
     client = mlflow.MlflowClient()
-    latest_version = client.get_latest_versions(model_name, stages=["Staging"])
+    latest_version = client.get_latest_versions(model_name, stages=["Production"])
     if not latest_version:
         latest_version = client.get_latest_versions(model_name, stages=["None"])
     return latest_version[0].version if latest_version else None
